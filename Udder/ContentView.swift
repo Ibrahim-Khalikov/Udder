539037c41712f26ed4a12519177a7320c30f7fea
Ibrahim Khalikov <starlink050@gmail.com>
2022-06-15 11:17:37 +0500

    Initial Commit

---
+++ Udder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Udder
+//
+//  Created by cloud_vfx on 15/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world! _ Another we  addedd")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Udder/UdderApp.swift
@@ -0,0 +1,17 @@
+//
+//  UdderApp.swift
+//  Udder
+//
+//  Created by cloud_vfx on 15/06/22.
+//
+
+import SwiftUI
+
+@main
+struct UdderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
