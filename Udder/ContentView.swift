3aa6dde2bfb528ba81b189da29a1e9078f17276d
Ibrahim Khalikov <starlink050@gmail.com>
2022-06-15 12:19:22 +0500

    Added  Potato Image

--- Udder/ContentView.swift
+++ Udder/ContentView.swift
@@ -11,6 +11,8 @@ struct ContentView: View {
     var body: some View {
         VStack{
         
+            Image("baked potato")
+            
         Text("Hello, world! _ Another we  addedd")
             .padding()
         
